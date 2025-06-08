5a3cb30fff96e35f165b0233ce7fb5dfc6162d30
Dan Hughes <dan@radixfoundation.org>
2025-06-08 16:18:10 +0100

    Added a crude and basic profiler to gossip processors
    Can remove at a later date.

--- core/src/main/java/org/radix/hyperscale/network/GossipInventory.java
+++ core/src/main/java/org/radix/hyperscale/network/GossipInventory.java
@@ -1,11 +1,59 @@
 package org.radix.hyperscale.network;
 
 import java.util.Collection;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
+import org.radix.hyperscale.Context;
 import org.radix.hyperscale.common.Primitive;
 import org.radix.hyperscale.crypto.Hash;
+import org.radix.hyperscale.logging.Logger;
+import org.radix.hyperscale.logging.Logging;
 
-public interface GossipInventory
+public abstract class GossipInventory
 {
-	public Collection<Hash> required(final Class<? extends Primitive> type, final Collection<Hash> items, final AbstractConnection connection) throws Throwable;
+	private static final Logger gossipLog = Logging.getLogger("gossip");
+
+	private final Context context;
+	private volatile long iterations; 
+	private volatile long duration;
+	
+	private final long reportInterval;
+	private final TimeUnit reportTimeUnit;
+	private volatile long lastReportAt;
+	
+	public GossipInventory(final Context context)
+	{
+		Objects.requireNonNull(context, "Context is null");
+		this.context = context;
+		
+		this.reportInterval = 60;
+		this.reportTimeUnit = TimeUnit.SECONDS;
+		this.lastReportAt = System.currentTimeMillis();
+	}
+
+	public final Collection<Hash> required(final Class<? extends Primitive> type, final Collection<Hash> items, final AbstractConnection connection) throws Throwable
+	{
+		final long start = System.nanoTime();
+		try
+		{
+			return process(type, items, connection);
+		}
+		finally
+		{
+			this.iterations++;
+			this.duration += (System.nanoTime() - start);
+			
+			if (System.currentTimeMillis() - this.lastReportAt >= TimeUnit.MILLISECONDS.convert(this.reportInterval, this.reportTimeUnit))
+			{
+				final double averageDurationNano = this.duration / this.iterations;
+				final double averageDurationMS = averageDurationNano / 1_000_000;
+				gossipLog.info(this.context.getName()+": Inventory processor for "+type.getSimpleName()+" "+this.iterations+" iterations duration="+TimeUnit.NANOSECONDS.toMillis(this.duration)+"ms average="+averageDurationMS+"ms");
+
+				this.lastReportAt = System.currentTimeMillis();
+			}
+		}
+	}
+	
+	public abstract Collection<Hash> process(final Class<? extends Primitive> type, final Collection<Hash> items, final AbstractConnection connection) throws Throwable;
 }
--- core/src/main/java/org/radix/hyperscale/network/GossipReceiver.java
+++ core/src/main/java/org/radix/hyperscale/network/GossipReceiver.java
@@ -1,10 +1,58 @@
 package org.radix.hyperscale.network;
 
 import java.util.Collection;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
+import org.radix.hyperscale.Context;
 import org.radix.hyperscale.common.Primitive;
+import org.radix.hyperscale.logging.Logger;
+import org.radix.hyperscale.logging.Logging;
 
-public interface GossipReceiver<T extends Primitive>
+public abstract class GossipReceiver<T extends Primitive>
 {
-	public void receive(final Collection<T> objects, final AbstractConnection connection) throws Throwable;
+	private static final Logger gossipLog = Logging.getLogger("gossip");
+
+	private final Context context;
+	private volatile long iterations; 
+	private volatile long duration;
+	
+	private final long reportInterval;
+	private final TimeUnit reportTimeUnit;
+	private volatile long lastReportAt;
+	
+	public GossipReceiver(final Context context)
+	{
+		Objects.requireNonNull(context, "Context is null");
+		this.context = context;
+		
+		this.reportInterval = 60;
+		this.reportTimeUnit = TimeUnit.SECONDS;
+		this.lastReportAt = System.currentTimeMillis();
+	}
+	
+	public final void receive(final Class<? extends Primitive> type, final Collection<T> items, final AbstractConnection connection) throws Throwable
+	{
+		final long start = System.nanoTime();
+		try
+		{
+			process(type, items, connection);
+		}
+		finally
+		{
+			this.iterations++;
+			this.duration += (System.nanoTime() - start);
+			
+			if (System.currentTimeMillis() - this.lastReportAt >= TimeUnit.MILLISECONDS.convert(this.reportInterval, this.reportTimeUnit))
+			{
+				final double averageDurationNano = this.duration / this.iterations;
+				final double averageDurationMS = averageDurationNano / 1_000_000;
+				gossipLog.info(this.context.getName()+": Receive processor for "+type.getSimpleName()+" "+this.iterations+" iterations duration="+TimeUnit.NANOSECONDS.toMillis(this.duration)+"ms average="+averageDurationMS+"ms");
+				
+				this.lastReportAt = System.currentTimeMillis();
+			}
+		}
+	}
+	
+	public abstract void process(final Class<? extends Primitive> type, final Collection<T> items, final AbstractConnection connection) throws Throwable;
 }
